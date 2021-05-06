f620e32036928522714c6d011d3b8b1e79821dae
ssihh <49466178+ssihh@users.noreply.github.com>
2021-05-06 23:27:35 +0900

    Add files via upload

---
+++ share.js
@@ -0,0 +1,33 @@
+Kakao.Link.createDefaultButton({
+    container: '#CONTAINER_ID',
+    objectType: 'feed',
+    content: {
+      title: 'MBTIjs',
+      description: 'MBTI 카드추천',
+      imageUrl:
+        'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+      link: {
+        mobileWebUrl: 'https://developers.kakao.com',
+        androidExecParams: 'test',
+      },
+    },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
+    },
+    buttons: [
+      {
+        title: '웹으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+      {
+        title: '앱으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+    ]
+  });
